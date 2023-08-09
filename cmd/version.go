dae01e1e8755a826918139bd332cdc6251d91c23
myyrakle <sssang97@naver.com>
2023-08-10 02:14:48 +0900

    feat: add version command

---
+++ cmd/version.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var Version = "0.1.0"
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "display version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("mongery version %s\n", Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
